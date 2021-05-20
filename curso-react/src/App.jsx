6f35f44260d52f8c043c8fdb2dab2de92397dd1f
Thaynan Saveli <thay.henze@hotmail.com>
2021-05-20 17:59:15 -0300

    Ajusets cards e herança de props

--- curso-react/src/App.jsx
+++ curso-react/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import NumeroAleatorio from './components/basicos/NumeroAleatorio'
+import Familia from './components/basicos/Familia'
+import Membro from './components/basicos/MembroFamilia'
 import Card from './components/layout/Card'
 
 import './index.css'
@@ -10,11 +12,26 @@ export default () => (
     <div id="app">
         <h1>Fundamentos 3</h1>
 
-        <Card titulo="Desafio Aleatorio">
-            <NumeroAleatorio min={1} max={20}/>
-        </Card>
+        <div className="cards">
+            <Card titulo="#04 - Componente com filhos" color="#00c8f8">
+                <Familia sobrenome="Silva">
+                    <Membro nome="Pedro"/>
+                    <Membro nome="Ana"/>
+                    <Membro nome="João"/>
+                </Familia>
+            </Card>
 
-        <ComParametro titulo="Componente com Título" subtitulo="Subtitulo do componente" />
-        <Primeiro/>
+            <Card titulo="#03 - Desafio Aleatorio" color="#fa6900">
+                <NumeroAleatorio min={1} max={20}/>
+            </Card>
+
+            <Card titulo="#02 - Componente com Parâmetro" color="#e94c6f">
+                <ComParametro titulo="Componente com Título" subtitulo="Subtitulo do componente" />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente" color="#e8b71a">
+                <Primeiro/>
+            </Card>
+        </div>
     </div>
 );
--- curso-react/src/components/layout/Card.jsx
+++ curso-react/src/components/layout/Card.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import '../../assets/css/Card.css'
 
 export default (props) => {
+
+    const estilo = {
+        backgroundColor: props.color || '#f00',
+        borderColor: props.color || '#f00',
+    }
+
     return(
-        <div>
-            <div className="card">
-                <div className="title">{props.titulo}</div>
-                <div className="content">
-                    {props.children}
-                </div>
-            </div>
+        <div className="card" style={estilo}>
+            <div className="title">{props.titulo}</div>
+            <div className="content">{props.children}</div>
         </div>
     )
 }
